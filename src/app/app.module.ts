2436a5f912dc20f3a67aac89eaf0199997a02875
Duván Smith Montenegro Roque <programador5@sypsolutions.com>
2021-02-11 11:55:15 -0500

    Se modificaron los componentes para ver su funcionamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { EjemploTirarComponent } from './ejemplo-tirar/ejemplo-tirar.component';
 
 @NgModule({
   declarations: [
-    AppComponent, EmpleadosComponent, EmpleadoComponent
+    AppComponent, EmpleadosComponent, EmpleadoComponent, EjemploTirarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ejemplo-tirar/ejemplo-tirar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EjemploTirarComponent } from './ejemplo-tirar.component';
+
+describe('EjemploTirarComponent', () => {
+  let component: EjemploTirarComponent;
+  let fixture: ComponentFixture<EjemploTirarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EjemploTirarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EjemploTirarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
