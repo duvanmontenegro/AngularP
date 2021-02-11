fc5cd78f495c910126028b676a18f8c00717e994
Duván Smith Montenegro Roque <programador5@sypsolutions.com>
2021-02-11 11:37:09 -0500

    Se crean componentes para pruebas (empleados)

---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-empleados",
+    templateUrl:"./empleados.component.html",
+    styleUrls:["./empleados.component.css"]
+})
+export class EmpleadosComponent{    //expor:permite aceder a esta clase desde fuera
+    
+} 
